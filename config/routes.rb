b050984d69e397a806a9c1ce1cdaf56053d01395
jeffrey-meesters <jjeffreymeesters@gmail.com>
2016-11-14 16:19:54 +0100

    Add user reference column to boards

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'pages/home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "home#index"
+  root to: "pages#home"
 
   devise_for :users
+
+  resources :boards
 
 end
