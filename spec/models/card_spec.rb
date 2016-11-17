f0a7691416b0b916953356a3dffd3a9b75df7fde
jeffrey-meesters <jjeffreymeesters@gmail.com>
2016-11-18 00:54:32 +0100

    Add model validation testing

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Card, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "is invalid without a title" do
+      card = Card.new(title: "")
+      card.valid?
+      expect(card.errors).to have_key(:title)
+    end
+  end
 end
